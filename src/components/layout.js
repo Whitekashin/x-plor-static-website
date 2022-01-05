9af8fd3b53565e2293d398b18c699a1c53cabe34
Whitekashin <patrick.reitbauer@web.de>
2022-01-05 10:10:02 +0100

    Initial commit

---
+++ src/components/layout.js
@@ -0,0 +1,19 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Navbar from "./Navbar"
+import Footer from "./Footer"
+import "./layout.css"
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import HeroSection from "../components/HeroSection"
+import Image from "../components/Image"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <HeroSection />
+    <Image />
+  </Layout>
+)
+
+export default IndexPage
